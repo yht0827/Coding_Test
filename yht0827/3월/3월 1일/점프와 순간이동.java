fb7e24906e9e6638cf9e6eb903924a6d9c004953
yht0827 <yangheetae0827@naver.com>
2024-02-29 17:50:34 +0900

    2024-03-01 점프와 순간이동

---
+++ yht0827/3월/3월 1일/점프와 순간이동.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public int solution(int n) {
+		int answer = 0;
+
+		while (n > 0) {
+			if (n % 2 == 1) {
+				n -= 1;
+				answer++;
+			}
+			n /= 2;
+		}
+
+		return answer;
+	}
+}
